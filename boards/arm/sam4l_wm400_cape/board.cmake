0a6c0278c300a58bd00f5137fc9b17b50be3cd3e
Gerson Fernando Budke <nandojve@gmail.com>
2020-04-21 18:46:02 -0300

    boards: arm: sam4l_wm400_cape: Fix reset after flash
    This ensures that USART will present right characters after TAP
    operations.
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- boards/arm/sam4l_wm400_cape/board.cmake
+++ boards/arm/sam4l_wm400_cape/board.cmake
@@ -1,4 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=atsam4lc4b" "--speed=4000")
+#board_runner_args(jlink "--iface=JTAG")
+#board_runner_args(jlink "--tool-opt=-jtagconf -1,-1")
+#board_runner_args(jlink "--tool-opt=-autoconnect 1")
+board_runner_args(jlink "--device=atsam4lc4b")
+board_runner_args(jlink "--speed=4000")
+board_runner_args(jlink "--reset-after-load")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
